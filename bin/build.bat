a0118c52de1eb5534b1fa24e88b4398350eb5cb9
pkoscielniak <pkoscielniak@rekord.com.pl>
2019-08-08 09:44:45 +0200

    new version built

--- bin/build.bat
+++ bin/build.bat
@@ -1,6 +1,6 @@
+SET CURRENT_DIR=%~dp0
 SET GOOS=windows
 SET GOARCH=amd64
-
 cd ../main
 
 go build -o ../bin/windows/MattermostBot.exe
@@ -9,3 +9,6 @@ SET GOOS=linux
 SET GOARCH=amd64
 
 go build -o ../bin/linux/MattermostBot
+
+SET GOOS=windows
+cd %CURRENT_DIR%
